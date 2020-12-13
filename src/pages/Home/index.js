7df915929dfa7b4f465cbc9be338c9ef444e6c2d
Luis Enrique <luyo.lemp@gmail.com>
2020-12-12 22:45:31 -0300

    remove imports

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -10,18 +10,13 @@ import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
-import MailIcon from "@material-ui/icons/Mail";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
-import AccountCircle from "@material-ui/icons/AccountCircle";
 import SearchIcon from "@material-ui/icons/Search";
 
 import {
   Container,
   Card,
-  CardActions,
-  CardContent,
-  Button,
   Grid,
   Paper,
   FormControl,
