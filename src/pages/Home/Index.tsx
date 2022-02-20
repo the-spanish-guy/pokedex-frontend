fbeed610a7c3dcda4f4f8236cf19e1b7fe9a4c7a
Luis Enrique Meza Paredes <luyo.lemp@gmail.com>
2022-02-20 17:55:25 -0300

    :sparkles: feat(page): add home page

--- src/pages/Home/Index.tsx
+++ src/pages/Home/Index.tsx
@@ -1,21 +1,30 @@
-import { Box, Center, Heading, HStack, Text } from '@chakra-ui/react'
-import LayoutComponent from '@/components/Layout'
-// import { ToggleThemeButton } from '@/components/ToggleTheme/Index'
-import { getInfoColors } from '@/utils/PokemonUtils'
 import { useLayoutEffect, useState } from 'react'
-import { getIconByType } from '@/utils/IconUtils'
+import { Box, Center, Heading, HStack, Image, Text } from '@chakra-ui/react'
+// import { ToggleThemeButton } from '@/components/ToggleTheme/Index'
+
+import useStore from '@/stores/useStore'
+import LayoutComponent from '@/components/Layout'
+import { getIconByName } from '@/utils/IconUtils'
+import { getInfoColors } from '@/utils/PokemonUtils'
+
+import Pokedex from '@/assets/cover.png'
 
 export function Home() {
   const [bgColor, setBgColor] = useState<string>()
+  const [darkColor, setDarkColor] = useState<string>()
   const [pokemonType, setPokemonType] = useState<string>()
-  const [pokemonIcon, setPokemonIcon] = useState<string>()
+  const [reverseColor, setReverseColor] = useState<string>()
+
+  const { setglobalBgColor } = useStore()
 
   const getInfos = () => {
-    const { color, type } = getInfoColors()
+    const { color, type, darkColor, reverseColor } = getInfoColors()
 
     setBgColor(color)
     setPokemonType(type)
-    setPokemonIcon(getIconByType(type))
+    setDarkColor(darkColor)
+    setglobalBgColor(color)
+    setReverseColor(reverseColor)
   }
   useLayoutEffect(() => {
     getInfos()
@@ -23,27 +32,92 @@ export function Home() {
 
   return (
     <LayoutComponent title="Home">
-      <HStack backgroundColor={bgColor} width="100%" height="100vh">
-        <Heading size="xs" fontWeight="medium">
-          scroll for more
-        </Heading>
-        <Center>
-          <Text fontFamily="Montserrat">A new away to see your pokémons</Text>
-          <Text fontFamily="Kabond" fontSize="228px">
-            Pokedex
+      <HStack
+        backgroundColor={bgColor}
+        width="100%"
+        height="100vh"
+        direction="row"
+        justifyContent="space-between"
+      >
+        <Box
+          display="flex"
+          flexDir="row"
+          alignItems="center"
+          alignSelf="self-end"
+          transform="rotate(90deg)"
+          width="14rem"
+          mb="109px"
+        >
+          <Heading fontSize="xs" fontWeight="medium">
+            scroll for more
+          </Heading>
+          <Box
+            as="div"
+            width="91px"
+            height="1px"
+            bgColor="whiteAlpha.500"
+            position="relative"
+          />
+        </Box>
+        <Center flexDirection="column">
+          <Text fontFamily="Montserrat" fontSize="32px">
+            A new away to see your pokémons
           </Text>
+          <Box position="relative">
+            <Text fontFamily="Kabond" fontSize="228px" color={reverseColor}>
+              Pokedéx
+            </Text>
+            <Image
+              src={Pokedex}
+              w="680px"
+              h="614px"
+              mt="-160px"
+              ml="140px"
+              alignSelf="center"
+            />
+          </Box>
           <Box
             as="span"
             display="flex"
-            flex={1}
-            width="250px"
-            bgColor="red.500"
+            bgColor="whiteAlpha.500"
             flexDir="row"
+            justifyContent="center"
+            alignItems="center"
+            borderRadius="60px"
+            p="8px  16px"
           >
-            <img src={pokemonIcon} width="20px" />
-            <Text>{pokemonType}</Text>
+            {getIconByName({
+              name: pokemonType as string,
+              fill: darkColor,
+              fontSize: '32px'
+            })}
+            <Text
+              ml="16px"
+              fontSize="md"
+              fontWeight="semibold"
+              color={darkColor}
+            >
+              {pokemonType}
+            </Text>
           </Box>
         </Center>
+        <Box
+          display="flex"
+          flexDir="row"
+          alignItems="center"
+          width="auto"
+          alignSelf="flex-end"
+        >
+          <Heading
+            fontSize="16px"
+            fontWeight="medium"
+            width="5rem"
+            mb="56px"
+            mr="69px"
+          >
+            PT---ENG
+          </Heading>
+        </Box>
       </HStack>
       {/* <ToggleThemeButton /> */}
     </LayoutComponent>
