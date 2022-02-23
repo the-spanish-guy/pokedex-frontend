20f6ce0b59d4c218023a45a6da0f71418d42d490
Luis Enrique Meza Paredes <luyo.lemp@gmail.com>
2022-02-23 19:49:39 -0300

    :sparkles: feat(components): using viewport in toggletheme componenet

--- src/components/ToggleTheme/Index.tsx
+++ src/components/ToggleTheme/Index.tsx
@@ -1,33 +1,47 @@
-import { Flex, useColorMode, useColorModeValue } from '@chakra-ui/react'
+import useStore from '@/stores/useStore'
+import {
+  Flex,
+  ScaleFade,
+  useColorMode,
+  useColorModeValue
+} from '@chakra-ui/react'
 import { DarkIcon, ElectricIcon } from '../Icons'
 
-export const ToggleThemeButton = () => {
+const Toggle = ({ fadeIn }: { fadeIn: boolean }) => {
   const { colorMode, toggleColorMode } = useColorMode()
 
   return (
-    <Flex
-      as="div"
-      position="fixed"
-      w="80px"
-      h="80px"
-      left="12px"
-      bottom="12px"
-      bgColor={useColorModeValue('#262B35', '#2C3444')}
-      rounded="9999px"
-      onClick={toggleColorMode}
-      boxShadow="lg"
-      cursor="pointer"
-      alignItems="center"
-      justifyContent="center"
-    >
-      {colorMode === 'dark' ? (
-        <ElectricIcon fontSize={32} fill="#F2D94E" />
-      ) : (
-        <DarkIcon
-          fontSize={32}
-          fill={useColorModeValue('#FFFFFF', '#262B35')}
-        />
-      )}
-    </Flex>
+    <ScaleFade initialScale={0.4} in={fadeIn}>
+      <Flex
+        as="div"
+        w="80px"
+        h="80px"
+        left="12px"
+        bottom="12px"
+        boxShadow="lg"
+        position="fixed"
+        rounded="9999px"
+        cursor="pointer"
+        alignItems="center"
+        justifyContent="center"
+        onClick={toggleColorMode}
+        bgColor={useColorModeValue('#262B35', '#2C3444')}
+      >
+        {colorMode === 'dark' ? (
+          <ElectricIcon fontSize={32} fill="#F2D94E" />
+        ) : (
+          <DarkIcon
+            fontSize={32}
+            fill={useColorModeValue('#FFFFFF', '#262B35')}
+          />
+        )}
+      </Flex>
+    </ScaleFade>
   )
+}
+
+export const ToggleThemeButton = () => {
+  const { inViewPort } = useStore()
+
+  return inViewPort ? <></> : <Toggle fadeIn={!inViewPort} />
 }
