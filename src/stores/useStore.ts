fb2230098702c16bf4635a238a2f5404deb6efdd
Luis Enrique Meza Paredes <luyo.lemp@gmail.com>
2022-02-23 19:50:30 -0300

    :sparkles: feat: update store hook

--- src/stores/useStore.ts
+++ src/stores/useStore.ts
@@ -3,12 +3,19 @@ import create from 'zustand'
 type State = {
   globalBgColor: string
   setglobalBgColor: (counter: string) => void
+  inViewPort: boolean
+  setInViewPort: (inViewPort: boolean) => void
 }
 
 const useStore = create<State>(set => ({
   globalBgColor: '',
   setglobalBgColor: bgColor => {
     set(state => ({ globalBgColor: bgColor }))
+  },
+
+  inViewPort: true,
+  setInViewPort: viewPort => {
+    set(state => ({ inViewPort: viewPort }))
   }
 }))
 
