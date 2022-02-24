2775f691d683d8978c38bc413ef1f79e48d333df
Luis Enrique Meza Paredes <luyo.lemp@gmail.com>
2022-02-23 23:53:35 -0300

    :lipstick: style: improve style and animation

--- src/components/ScrollToTop/Index.tsx
+++ src/components/ScrollToTop/Index.tsx
@@ -1,5 +1,6 @@
+import { motion } from 'framer-motion'
 import useStore from '@/stores/useStore'
-import { Flex, chakra, ScaleFade } from '@chakra-ui/react'
+import { chakra } from '@chakra-ui/react'
 import { IconArrowNarrowUp } from '@tabler/icons'
 
 const ChakraIconSrrowNarrowUp = chakra(IconArrowNarrowUp)
@@ -10,29 +11,33 @@ const Scroll = ({ fadeIn, color }: { fadeIn: boolean; color: string }) => {
     window.scroll({ top: 0, left: 0, behavior: 'smooth' })
   }
   return (
-    <ScaleFade initialScale={0.4} in={fadeIn}>
-      <Flex
-        as="div"
-        w="80px"
-        h="80px"
-        right="12px"
-        bottom="12px"
-        boxShadow="lg"
-        bgColor={color}
-        position="fixed"
-        rounded="9999px"
-        cursor="pointer"
-        alignItems="center"
-        onClick={scrollToTop}
-        justifyContent="center"
-      >
-        <ChakraIconSrrowNarrowUp
-          size="60px"
-          strokeWidth="1.5px"
-          color="whiteAlpha.500"
-        />
-      </Flex>
-    </ScaleFade>
+    <motion.div
+      style={{
+        width: '80px',
+        right: '12px',
+        bottom: '12px',
+        height: '80px',
+        display: 'flex',
+        boxShadow: 'lg',
+        position: 'fixed',
+        cursor: 'pointer',
+        alignItems: 'center',
+        backgroundColor: color,
+        borderRadius: '9999px',
+        justifyContent: 'center'
+      }}
+      onClick={scrollToTop}
+      initial={{ scale: 0 }}
+      animate={{ scale: 1 }}
+      whileInView={{ scale: 1 }}
+      transition={{ type: 'spring', stiffness: 300 }}
+    >
+      <ChakraIconSrrowNarrowUp
+        size="60px"
+        strokeWidth="1.5px"
+        color="whiteAlpha.500"
+      />
+    </motion.div>
   )
 }
 
