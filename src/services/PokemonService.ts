70535d66238fe25a720f4d4803ec3c68b38bda40
Luis Enrique Meza Paredes <luyo.lemp@gmail.com>
2022-02-25 11:52:00 -0300

    :sparkles: feat: search pokemon by name or id

--- src/services/PokemonService.ts
+++ src/services/PokemonService.ts
@@ -1,14 +1,24 @@
 import BasicService from './HttpServices'
 import { ResultPokemon } from '@/interfaces/ResultPokemonApiInterface'
 
+type PokemonsProps = {
+  offset?: number
+  id?: number | string
+}
+
 class PokemonService extends BasicService {
   constructor() {
     super(process.env.VITE_POKEMON_DOMAIN as string)
   }
 
-  public async getPokemons(offset = 0): Promise<ResultPokemon[]> {
+  public async getPokemons({
+    offset = 0,
+    id
+  }: PokemonsProps): Promise<ResultPokemon[]> {
     return (
-      await this.connection.get<ResultPokemon[]>(`/pokemons?offset=${offset}`)
+      await this.connection.get<ResultPokemon[]>(
+        `/pokemons?offset=${offset}&id=${id || ''}`
+      )
     ).data
   }
 }
