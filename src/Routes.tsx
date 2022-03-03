fe32fd8a2b93a188434ab5403ef87213d915823d
Luis Enrique Meza Paredes <luyo.lemp@gmail.com>
2022-03-02 21:44:14 -0300

    :sparkles: feat(routes): add /pokemon:id route

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -2,12 +2,14 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 import { Home } from '@/pages/Home/Index'
 import { NotFound } from './pages/NotFound/Index'
+import { Pokemon } from '@/pages/Pokemon/Index'
 
 export function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/pokemon/:id" element={<Pokemon />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
